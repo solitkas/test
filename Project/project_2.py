d20f72b5e3b645f0a258a54c0dfce845eb8e13dd
solitkas <sasha.zetix.228@gmail.com>
2022-12-09 12:24:19 +0200

    project_2

---
+++ Project/project_2.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import List, Any
+
+test_words = ["tea", "eat", "bat", "ate", "arc", "car"]
+
+
+
+def group_anagrams(words: List[str]) -> [list, Any]:
+    dfdict = defaultdict(list)
+    for i in words:
+        sorted_i = " ".join(sorted(i))
+        dfdict[sorted_i].append(i)
+    return dfdict.values()
+
+
+def anagrams(words: List[str]) -> dict:
+    array = {}
+    for i in words:
+        sorted_i = " ".join(sorted(i))
+        array[sorted_i] = array.get(sorted_i, []) + [i]
+    return array
+
+
+def info():
+    """
+    that's how it works sorted() --> [a a s s]
+    """
+    a = "sasa"
+    print(sorted(a))
+    print(" ".join(sorted(a)))
+
+
+if __name__ == "__main__":
+    print(group_anagrams(test_words))
+    print(anagrams(test_words))
+
