b9aff4f4aff3806e10783469133fb680de43af14
solitkas <sasha.zetix.228@gmail.com>
2022-12-12 14:32:05 +0200

    benchmark

---
+++ Project/project_4.py
@@ -0,0 +1,33 @@
+def benchmark(func):
+    import time
+    from functools import wraps
+
+    @wraps(func)
+    def wrapper():
+        """
+        Декоратор для тайминг-кода
+        """
+        start = time.monotonic()
+        print(func())
+        run_time = time.monotonic() - start
+        print(f"Время выполнения {run_time:.2f} секунд /ы.")  # :.2f - сколько цифр после запятой 0.00
+
+    return wrapper
+
+
+@benchmark
+def test_func() -> str:
+    import time
+    word = "Artificial Intelligence"
+    text = word.split()
+    a = " "
+    for i in text:
+        a = a + str(i[0]).upper()
+    time.sleep(2)
+    return a.strip()
+
+
+if __name__ == '__main__':
+    test_func()
+
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
-
-
-
+import time
+start_time = time.monotonic()
+time.sleep(4)
+print(time.monotonic()-start_time)
 
 
