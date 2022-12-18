9af63362ad9c32c4d2fb2eff156a3540fdca24c9
solitkas <sasha.zetix.228@gmail.com>
2022-12-18 14:18:23 +0200

    Scraping GitHub Profile

---
+++ Project/project_8.py
@@ -0,0 +1,12 @@
+# pip install beautifulsoup4
+# pip install requests
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+github_profile = "https://github.com/solitkas"
+req = requests.get(github_profile)
+scraper = bs(req.content, "html.parser")
+profile_picture = scraper.find("img", {"alt": "Avatar"})["src"]
+print(profile_picture)
+
