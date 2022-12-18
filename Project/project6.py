4c2564fb0aea57e4c34c1cd127f96632c2c2aa38
solitkas <sasha.zetix.228@gmail.com>
2022-12-18 13:51:30 +0200

    Print Emojis using Python

---
+++ Project/project6.py
@@ -0,0 +1,10 @@
+import emoji
+
+
+def Emojis_using():
+    """
+    pip install emoji
+    https://carpedm20.github.io/emoji/ - ссылка на все emoji
+    """
+    print(emoji.emojize(":broccoli:hi:Santa_Claus_medium_skin_tone:"))
+    print(emoji.emojize("Some people have a very sensitive heart:red_heart:, please be kind with them.:hibiscus:"))
