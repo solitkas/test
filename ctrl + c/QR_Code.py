f5be42e028bc0f6bb235ae44e9b2c21b2f21772f
solitkas <sasha.zetix.228@gmail.com>
2022-12-25 16:08:18 +0200

    QR_CODE

--- ctrl + c/QR_Code.py
+++ ctrl + c/QR_Code.py
@@ -2,19 +2,17 @@
 # pip install pypng
 
 import pyqrcode
+
 link = "https://github.com/solitkas"
 qr_code = pyqrcode.create(link)
 qr_code.png("test.png", scale=5)
 
 
-
 # pip install pyzbar
 # pip install pillow
 def Decode(png: str):
-# png: "test.png"
+    # png: "test.png"
     from pyzbar.pyzbar import decode
     from PIL import Image
     decode_qr = decode(Image.open(png))
     print(decode_qr[0].data.decode('ascii'))
-
-
